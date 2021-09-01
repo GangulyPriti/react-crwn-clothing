883c32e57a8eb0a4814e99e28f77b59a5bc28758
GangulyPriti <mondal.18riya@gmail.com>
2021-09-01 19:41:33 +0530

    added collection preview and item

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import './App.css';
 // import ComposeEmail from './composeEmail/compose-email.component';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homePage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className='App'>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop/hats' component={ShopPage} />
       </Switch>
       {/* <ComposeEmail /> */}
     </div>
